e39ae57bf7601122704b6d6745aa0bf441a42b9a
andreacagnina <amirkal@libero.it>
2024-09-24 18:44:49 +0200

    [feat] footer done

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -9,17 +9,21 @@
                                 dc comics
                             </h4>
                             <ul class="my-3">
-                                <li v-for="(label, index) in DcComics" :key="`DcC-${index}`"><a :href="label.url">
-                                        {{ label . label }}</a>
-                                </li>
+                                <li><a href="">Characters</a></li>
+                                <li><a href="">Comics</a></li>
+                                <li><a href="">Movies</a></li>
+                                <li><a href="">TV</a></li>
+                                <li><a href="">Games</a></li>
+                                <li><a href="">Videos</a></li>
+                                <li><a href="">News</a></li>
                             </ul>
 
                             <h4>
                                 shop
                             </h4>
                             <ul class="my-3">
-                                <li v-for="(label, index) in shop" :key="`shop-${index}`"><a
-                                        :href="label.url">{{ label . label }}</a></li>
+                                <li><a href="">Shop DC</a></li>
+                                <li><a href="">Shop DC Collectibles</a></li>
                             </ul>
                         </div>
                     </div>
@@ -27,8 +31,17 @@
                         <div class="content">
                             <h4>dc</h4>
                             <ul class="my-3">
-                                <li v-for="(label, index) in Dc" :key="`dc-${index}`"><a
-                                        :href="label.url">{{ label . label }}</a></li>
+                                <li><a href="">Terms Of Use</a></li>
+                                <li><a href="">Privacy policy(New)</a></li>
+                                <li><a href="">SAdd Choices</a></li>
+                                <li><a href="">Advertising</a></li>
+                                <li><a href="">Jobs</a></li>
+                                <li><a href="">Subscriptions</a></li>
+                                <li><a href="">Talent Workshops</a></li>
+                                <li><a href="">CPSC Certificates</a></li>
+                                <li><a href="">Ratings</a></li>
+                                <li><a href="">Shop Help</a></li>
+                                <li><a href="">Contact Us</a></li>
                             </ul>
                         </div>
                     </div>
@@ -36,13 +49,16 @@
                         <div class="content">
                             <h4>sites</h4>
                             <ul class="my-3">
-                                <li v-for="(label, index) in sites" :key="`sites-${index}`"><a
-                                        :href="label.url">{{ label . label }}</a></li>
+                                <li><a href="">Characters</a></li>
+                                <li><a href="">DC</a></li>
+                                <li><a href="">MAD Magazine</a></li>
+                                <li><a href="">DC Kids</a></li>
+                                <li><a href="">DC Universe</a></li>
+                                <li><a href="">DC Power Visa</a></li>
                             </ul>
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
@@ -56,12 +72,19 @@
                     <div class="content">
                         <button type="button" class="butn">SIGN-UP-NOW!</button>
                     </div>
-
                     <div class="content d-flex align-item-center">
                         <ul class="d-flex align-items-center my-0">
                             <li><a class="fw-bold" href="">FOLLOW US</a></li>
-                            <li v-for="(icon, index) in faIcons" :key="`FaFo-${index}`"><a href=""><img
-                                        :src="icon.url" alt=""></a></li>
+                            <li><a href=""><img src="{{ Vite::asset('resources/images/footer-facebook.png') }}"
+                                        alt=""></a></li>
+                            <li><a href=""><img src="{{ Vite::asset('resources/images/footer-twitter.png') }}"
+                                        alt=""></a></li>
+                            <li><a href=""><img src="{{ Vite::asset('resources/images/footer-youtube.png') }}"
+                                        alt=""></a></li>
+                            <li><a href=""><img src="{{ Vite::asset('resources/images/footer-pinterest.png') }}"
+                                        alt=""></a></li>
+                            <li><a href=""><img src="{{ Vite::asset('resources/images/footer-periscope.png') }}"
+                                        alt=""></a></li>
                         </ul>
                     </div>
                 </div>
