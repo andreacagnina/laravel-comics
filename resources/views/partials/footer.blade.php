7e498ef718d3b0d8d26da8b23c08fbe7c3d1427e
andreacagnina <amirkal@libero.it>
2024-09-24 18:24:49 +0200

    [feat] header done

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,71 @@
-<div class="container">
-    <div class="row">
-        <div class="col-12"></div>
+<div class="top-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 ">
+                <div class="content d-flex py-5 bg-image-logo">
+                    <div class="col-2">
+                        <div class="content">
+                            <h4>
+                                dc comics
+                            </h4>
+                            <ul class="my-3">
+                                <li v-for="(label, index) in DcComics" :key="`DcC-${index}`"><a :href="label.url">
+                                        {{ label . label }}</a>
+                                </li>
+                            </ul>
+
+                            <h4>
+                                shop
+                            </h4>
+                            <ul class="my-3">
+                                <li v-for="(label, index) in shop" :key="`shop-${index}`"><a
+                                        :href="label.url">{{ label . label }}</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-2">
+                        <div class="content">
+                            <h4>dc</h4>
+                            <ul class="my-3">
+                                <li v-for="(label, index) in Dc" :key="`dc-${index}`"><a
+                                        :href="label.url">{{ label . label }}</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-2">
+                        <div class="content">
+                            <h4>sites</h4>
+                            <ul class="my-3">
+                                <li v-for="(label, index) in sites" :key="`sites-${index}`"><a
+                                        :href="label.url">{{ label . label }}</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="bottom-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="content">
+                        <button type="button" class="butn">SIGN-UP-NOW!</button>
+                    </div>
+
+                    <div class="content d-flex align-item-center">
+                        <ul class="d-flex align-items-center my-0">
+                            <li><a class="fw-bold" href="">FOLLOW US</a></li>
+                            <li v-for="(icon, index) in faIcons" :key="`FaFo-${index}`"><a href=""><img
+                                        :src="icon.url" alt=""></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,22 +1,26 @@
-<div class="container-fluid">
+<div class="container my-3">
     <div class="row">
         <div class="col-12">
-            <div>
-                {{-- <img src="{{ vite::asset('resources/img/') }}" alt="Logo"> --}}
-                <ul class="list-unstyled d-flex">
-                    <li>
-                        <a class="text-decoration-none" href="{{ route('homepage') }}">Home</a>
-                    </li>
-                    <li>
-                        <a class="text-decoration-none" href=""></a>
-                    </li>
-                    <li>
-                        <a class="text-decoration-none" href=""></a>
-                    </li>
-                    <li>
-                        <a class="text-decoration-none" href=""></a>
-                    </li>
-                </ul>
+            <div class="d-flex justify-content-around">
+                <div class="content">
+                    <a href="#">
+                        <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="">
+                    </a>
+                </div>
+                <div class="content d-flex align-item-center">
+                    <ul class="d-flex my-0">
+                        <li class="fw-bold"><a href="">characters</a></li>
+                        <li class="fw-bold"><a href="">comics</a></li>
+                        <li class="fw-bold"><a href="">movie</a></li>
+                        <li class="fw-bold"><a href="">tv</a></li>
+                        <li class="fw-bold"><a href="">games</a></li>
+                        <li class="fw-bold"><a href="">collectibles</a></li>
+                        <li class="fw-bold"><a href="">videos</a></li>
+                        <li class="fw-bold"><a href="">fans</a></li>
+                        <li class="fw-bold"><a href="">news</a></li>
+                        <li class="fw-bold"><a href="">shop</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
